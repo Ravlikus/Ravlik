b065dd7a246e35113122a7e19741a5cf49143d63
Ravlik <ravlikfure@gmail.com>
2019-08-30 11:00:03 +0400

    Creating Types tastk solved

---
+++ CreatingTypesSolution/CreatingTypesSolution/BubbleSortSolution.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using NUnit.Framework.Internal;
+
+namespace CreatingTypesSolution
+{
+    public static class BubbleSortSolution
+    {
+        public enum SortByParam
+        {
+            BySum,
+            ByMaxValue,
+            ByMinValue
+        }
+
+        public enum OrderByParam
+        {
+            Ascending,
+            Descending
+        }
+
+        /// <summary>
+        /// Метод сортировки двумерной матрицы
+        /// </summary>
+        /// <param name="matrix">Матрица</param>
+        /// <param name="sortBy">Парамметр сортировки (по сумме строки,максимальному/минимальному значению в строке)</param>
+        /// <param name="orderBy">Парамметр результирующего порядка(по возрастанию/убыванию)</param>
+        public static int[][] SortMatrix(int[][] matrix, SortByParam sortBy, OrderByParam orderBy) 
+        {
+            var matrixRowsData = new RowData[matrix.Length];
+            for (int i = 0; i < matrix.Length; i++)
+            {
+                var rowData= new RowData();
+                rowData.NumberByOrder = i;
+                switch (sortBy)
+                {
+                    case SortByParam.ByMaxValue:
+                        rowData.SortValue = matrix[i].Max();
+                        break;
+                    case SortByParam.ByMinValue:
+                        rowData.SortValue = matrix[i].Min();
+                        break;
+                    case SortByParam.BySum:
+                        rowData.SortValue = matrix[i].Sum();
+                        break;
+                }
+
+                matrixRowsData[i] = rowData;
+            }
+            
+            for (int i = 1; i < matrixRowsData.Length; i++)
+            {
+                for (int j = i; j > 0; j--)
+                {
+                    if (orderBy == OrderByParam.Ascending)
+                        if (matrixRowsData[j].SortValue < matrixRowsData[j - 1].SortValue)
+                        {
+                            var container = matrixRowsData[j];
+                            matrixRowsData[j] = matrixRowsData[j - 1];
+                            matrixRowsData[j - 1] = container;
+                        }
+                    if (orderBy == OrderByParam.Descending)
+                        if (matrixRowsData[j].SortValue > matrixRowsData[j - 1].SortValue)
+                        {
+                            var container = matrixRowsData[j];
+                            matrixRowsData[j] = matrixRowsData[j - 1];
+                            matrixRowsData[j - 1] = container;
+                        }
+                }
+            }
+
+            var result = new int[matrix.Length][];
+            for (int i = 0; i < matrix.Length; i++)
+            {
+                result[i] = matrix[matrixRowsData[i].NumberByOrder];
+            }
+
+            return result;
+        }
+
+        private class RowData
+        {
+            public int NumberByOrder;
+            public int SortValue;
+        }
+    }
+
+    [TestFixture]
+    public class BubbleSortTests
+    {
+        static int[][] MatrixExample = new int[][] {new int[]{1,2,3}, new int[] { 2,3,4}, new int[] { 3,4,5}};
+
+        [TestCase(BubbleSortSolution.SortByParam.ByMaxValue,BubbleSortSolution.OrderByParam.Ascending, ExpectedResult = true)]
+        [TestCase(BubbleSortSolution.SortByParam.ByMaxValue,BubbleSortSolution.OrderByParam.Descending, ExpectedResult = true)]
+        [TestCase(BubbleSortSolution.SortByParam.ByMinValue, BubbleSortSolution.OrderByParam.Ascending, ExpectedResult = true)]
+        [TestCase(BubbleSortSolution.SortByParam.ByMinValue, BubbleSortSolution.OrderByParam.Descending, ExpectedResult = true)]
+        [TestCase(BubbleSortSolution.SortByParam.BySum, BubbleSortSolution.OrderByParam.Ascending, ExpectedResult = true)]
+        [TestCase(BubbleSortSolution.SortByParam.BySum, BubbleSortSolution.OrderByParam.Descending, ExpectedResult = true)]
+        public bool SimpleSortTests( BubbleSortSolution.SortByParam sortBy, BubbleSortSolution.OrderByParam orderBy)
+        {
+            var matrix = MatrixExample;
+            matrix = BubbleSortSolution.SortMatrix(matrix,sortBy,orderBy);
+            switch (sortBy)
+            {
+                case BubbleSortSolution.SortByParam.ByMaxValue:
+                    if (orderBy == BubbleSortSolution.OrderByParam.Ascending)
+                        return CompareMatrix(matrix,MatrixExample.OrderBy(x => x.Max()).ToArray());
+                    else return CompareMatrix(matrix,MatrixExample.OrderByDescending(x => x.Max()).ToArray());
+                case BubbleSortSolution.SortByParam.ByMinValue:
+                    if (orderBy == BubbleSortSolution.OrderByParam.Ascending)
+                        return CompareMatrix(matrix, MatrixExample.OrderBy(x => x.Min()).ToArray());
+                    else return CompareMatrix(matrix, MatrixExample.OrderByDescending(x => x.Min()).ToArray());
+                case BubbleSortSolution.SortByParam.BySum:
+                    if (orderBy == BubbleSortSolution.OrderByParam.Ascending)
+                        return CompareMatrix(matrix, MatrixExample.OrderBy(x => x.Sum()).ToArray());
+                    else return CompareMatrix(matrix, MatrixExample.OrderByDescending(x => x.Sum()).ToArray());
+                default: return false;
+            }
+        }
+
+        private bool CompareMatrix(int[][] first, int[][] second)
+        {
+            var flag = true;
+            for (int i = 0; i < first.Length; i++)
+            {
+                for (int j = 0; j < first[i].Length; j++)
+                {
+                    if (first[i][j] != second[i][j])
+                    {
+                        flag = false;
+                        break;
+                    }
+                }
+                    if (!flag)
+                        break;
+            }
+
+            return flag;
+        }
+    }
+}
---
+++ CreatingTypesSolution/CreatingTypesSolution/FindNthRootSolution.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using NUnit.Framework.Internal;
+
+namespace CreatingTypesSolution
+{
+    public static class FindNthRootSolution
+    {
+        /// <summary>
+        /// Собственно реализация метода Ньютона.
+        /// </summary>
+        /// <param name="a">Число под корнем</param>
+        /// <param name="n">Степень корня</param>
+        /// <param name="e">Точность</param>
+        /// <returns></returns>
+        public static double FindNthRoot(double a, int n, double e)
+        {
+            if (a < 0 && n%2 == 0 ||
+                n < 0 || //тут не уверен
+                e <= 0) 
+                throw new ArgumentException();
+            var current = a;
+            double prev;
+            do
+            {
+                prev = current;
+                current = ((n - 1) * current + a / (Math.Pow(current, n - 1))) / n; //Формула из википедии
+            } while (Math.Abs(prev - current) > e);
+            
+            return Math.Round(current,(int)Math.Log(e,0.1));
+        }
+    }
+
+    [TestFixture]
+    public class FindNthRootTests
+    {
+        [TestCase(1, 5, 0.0001, ExpectedResult = 1)]
+        [TestCase(8, 3, 0.0001, ExpectedResult = 2)]
+        [TestCase(0.001, 3, 0.0001, ExpectedResult = 0.1)]
+        [TestCase(0.04100625, 4, 0.0001, ExpectedResult = 0.45)]
+        [TestCase(8, 3, 0.0001, ExpectedResult = 2)]
+        [TestCase(0.0279936, 7, 0.0001, ExpectedResult = 0.6)]
+        [TestCase(0.0081, 4, 0.1, ExpectedResult = 0.3)]
+        [TestCase(-0.008, 3, 0.1, ExpectedResult = -0.2)]
+        [TestCase(0.004241979, 9, 0.00000001, ExpectedResult = 0.545)]
+        public double SimpleFinding(double a, int n, double e)
+        {
+            return FindNthRootSolution.FindNthRoot(a, n, e);
+        }
+
+        [TestCase(-0.01,2,0.0001, ExpectedResult = typeof(ArgumentException))]
+        [TestCase(0.001, -2, 0.0001, ExpectedResult = typeof(ArgumentException))]
+        [TestCase(0.001, 2, -1, ExpectedResult = typeof(ArgumentException))]
+        public Type CheckExceptions(double a, int n, double e)
+        {
+            try
+            {
+                FindNthRootSolution.FindNthRoot(a, n, e);
+            }
+            catch (Exception ex)
+            {
+                return ex.GetType();
+            }
+            throw new Exception("Exception was not throwed");
+        }
+    }
+}
