e8d78fd784d7fc605ba78164ebe75913704dabe4
Ravil <ravlikfure@gmail.com>
2019-12-08 15:51:29 +0400

    Task 6-1: solution realised

---
+++ Methods in details solution/MethodsSol/MethodsSol/Solution.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Text;
+
+namespace MethodsSol
+{
+    public static class DoubleExtentions
+    {
+        //Чет решение немного затянулось (тут всё ещё много костылей, но стандартные тесты вродь проходят)
+
+        private static int manticeLenght = 52;
+        private static int expLenght = 11;
+
+        public static string ToBinaryString(this Double value)
+        {
+            if (value == double.PositiveInfinity) return "Positive Infinity";
+            if (value == double.NegativeInfinity) return "Negative Infinity";
+            //var result = value>0?"0":"1";
+            var sign = value >= 0 ? "0" : "1";
+            int exp;
+            var normilizedValue = (decimal)GetExp(value,  out exp);
+            var floatPart = normilizedValue<1?normilizedValue*2:(normilizedValue-1)*2;
+            var mantice = new StringBuilder(manticeLenght);
+            var flag = false;
+            for (int i = 0; i < manticeLenght; i++)
+            {
+                if (floatPart > 1)
+                {
+                    mantice.Append(1);
+                    floatPart -= 1;
+                    flag = true;
+                }
+                else
+                {
+                    if (!flag && i == manticeLenght - 1)
+                        mantice.Append(1);
+                    else mantice.Append(0);
+                }
+
+                floatPart *= 2;
+            }
+            exp += (int)Math.Pow(2,expLenght-1) -1;
+            var strExp = new StringBuilder(expLenght);
+
+            var counter = 0;
+            while (exp > 1 || counter < expLenght)
+            {
+                if (exp % 2 > 0) strExp.Insert(0,1);
+                else strExp.Insert(0,0);
+                exp /= 2;
+                if (counter >= expLenght) throw new ArithmeticException("Exp is out of range");
+                counter++;
+            }
+            return sign + strExp.ToString() + mantice.ToString();
+        }
+
+        private static double GetExp(double value,  out int exp)
+        {
+            value = Math.Abs(value);
+            exp = 0;
+            while (value >= 2 || value < 1)
+            {
+                if (value >= 2)
+                {
+                    exp++;
+                    value /= 2;
+                }
+                else
+                {
+                    exp--;
+                    value *= 2;
+                }
+            }
+
+            return value;
+        }
+    }
+}
---
+++ Methods in details solution/MethodsSol/MethodsSol/Tests.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using NUnit.Framework;
+using NUnit.Framework.Internal;
+
+namespace MethodsSol
+{
+
+    [TestFixture]
+    public class DoubleExtentionsTests
+    {
+        [TestCase(-255.255, ExpectedResult = "1100000001101111111010000010100011110101110000101000111101011100")]
+        [TestCase(255.255, ExpectedResult = "0100000001101111111010000010100011110101110000101000111101011100")]
+        [TestCase(4294967295.0, ExpectedResult = "0100000111101111111111111111111111111111111000000000000000000101")]
+        [TestCase(0.5, ExpectedResult = "0011111111100000000000000000000000000000000000000000000000000001")]
+        [TestCase(double.MinValue, ExpectedResult = "1111111111101111111111111111111111111111111111111111111111111111")]
+        [TestCase(double.MaxValue, ExpectedResult = "0111111111101111111111111111111111111111111111111111111111111111")]
+        [TestCase(double.Epsilon, ExpectedResult = "0000000000000000000000000000000000000000000000000000000000000001")]
+        [TestCase(double.PositiveInfinity, ExpectedResult = "Positive Infinity")]
+        [TestCase(double.NegativeInfinity, ExpectedResult = "Negative Infinity")]
+        public static string SimpleTest(Double input)
+        {
+            return input.ToBinaryString();
+        }
+    }
+}
