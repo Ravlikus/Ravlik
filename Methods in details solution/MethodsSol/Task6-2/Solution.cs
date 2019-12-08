64121099d35ccdf4260ed1f7e76fb98de607817c
Ravil <ravlikfure@gmail.com>
2019-12-08 16:49:07 +0400

    Task6-2: solution realised

---
+++ Methods in details solution/MethodsSol/Task6-2/Solution.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task6_2
+{
+    public static class EuclidianGCDFinder // НОД 
+    {
+        public static TimeSpan GetGCDFindingTiming(int a, int b, params int[] values)
+        {
+            var timer = new Stopwatch();
+            timer.Start();
+            var s = FindGCD(a, b, values);
+            timer.Stop();
+            return timer.Elapsed;
+        }
+
+        public static int FindGCD(int a, int b,params int[]  values)
+        {
+            var result = FindGCD(a, b);
+            for (int i = 0; i < values.Length; i++)
+            {
+                result = FindGCD(result, values[i]);
+            }
+            return result;
+        }
+
+        public static int FindGCD(int a, int b)
+        {
+            if (a == 0) return b;
+            if (b == 0) return a;
+            a = Math.Abs(a);
+            b = Math.Abs(b);
+            if (a == b) return a;
+            var min = Math.Min(a, b);
+            var diff = Math.Abs(a - b);
+            return FindGCD(min, diff);
+        }
+    }
+
+    public static class SteinGCDFinder
+    {
+        public static TimeSpan GetGCDFindingTiming(int a, int b, params int[] values)
+        {
+            var timer = new Stopwatch();
+            timer.Start();
+            var s = FindGCD(a, b, values);
+            timer.Stop();
+            return timer.Elapsed;
+        }
+
+        public static int FindGCD(int a, int b, params int[] values)
+        {
+            var result = FindGCD(a, b);
+            for (int i = 0; i < values.Length; i++)
+            {
+                result = FindGCD(result, values[i]);
+            }
+            return result;
+        }
+
+        public static int FindGCD(int a, int b)
+        {
+            if (a == 0) return b;
+            if (b == 0) return a;
+            if (a == b) return a;
+            if (a == 1) return 1;
+            if (b == 1) return 1;
+            if (a % 2 == 0 && b % 2 == 0) return 2 * FindGCD(a / 2, b / 2);
+            if (a % 2 == 0 && b % 2 != 0) return FindGCD(a / 2, b);
+            if (a % 2 != 0 && b % 2 == 0) return FindGCD(a, b / 2);
+            if (a > b) return FindGCD((a - b) / 2, b);
+            return FindGCD((b - a) / 2, a);
+        }
+    }
+}
---
+++ Methods in details solution/MethodsSol/Task6-2Tests/Tests.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Task6_2;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task6_2.Tests
+{
+    [TestClass()]
+    public class SolutionTests
+    {
+        [TestMethod()]
+        public void EuclidianAlgSimpleTests()
+        {
+            Assert.AreEqual(2,EuclidianGCDFinder.FindGCD(4,6));
+            Assert.AreEqual(2, EuclidianGCDFinder.FindGCD(4, 6, 8));
+            Assert.AreEqual(8, EuclidianGCDFinder.FindGCD(8, 8, 8));
+            Assert.AreEqual(0, EuclidianGCDFinder.FindGCD(0, 0, 0));
+            Assert.AreEqual(1, EuclidianGCDFinder.FindGCD(0, 0, 1));
+            Assert.AreEqual(2, EuclidianGCDFinder.FindGCD(-4, 6, 8));
+            Assert.AreEqual(1, EuclidianGCDFinder.FindGCD(15, 18, 10, 5));
+        }
+
+        [TestMethod()]
+        public void SteinsAlgSimpleTests()
+        {
+            Assert.AreEqual(2, SteinGCDFinder.FindGCD(4, 6));
+            Assert.AreEqual(2, SteinGCDFinder.FindGCD(4, 6, 8));
+            Assert.AreEqual(8, SteinGCDFinder.FindGCD(8, 8, 8));
+            Assert.AreEqual(0, SteinGCDFinder.FindGCD(0, 0, 0));
+            Assert.AreEqual(1, SteinGCDFinder.FindGCD(0, 0, 1));
+            Assert.AreEqual(2, SteinGCDFinder.FindGCD(-4, 6, 8));
+            Assert.AreEqual(1, SteinGCDFinder.FindGCD(15, 18, 10, 5));
+        }
+    }
+}
