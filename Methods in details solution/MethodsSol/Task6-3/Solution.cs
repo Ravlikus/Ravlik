382ae080810e122a89b44981316953a9add6ceeb
Ravil <ravlikfure@gmail.com>
2019-12-08 18:15:02 +0400

    Task6-3: solution realised

---
+++ Methods in details solution/MethodsSol/Task6-3/Solution.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task6_3
+{
+    public static class NullableExtentions
+    {
+        public static bool IsNull<T>(this T obj)
+        {
+            return /*(default(T).Equals(null) && obj.Equals(default(T))) || */(object)obj is null;
+        }
+    }
+}
---
+++ Methods in details solution/MethodsSol/Task6-3/Tests.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using NUnit.Framework.Internal;
+
+namespace Task6_3
+{
+    [TestFixture]
+    public class NulleableExtentionsTests
+    {
+        [TestCase(15, ExpectedResult = false)]
+        [TestCase(null, ExpectedResult = true)]
+        public static bool SimpleIntTest(int? s)
+        {
+             return s.IsNull();
+        }
+
+        [TestCase(15.0, ExpectedResult = false)]
+        [TestCase(null, ExpectedResult = true)]
+        public static bool SimpleDoubleTest(double? s)
+        {
+            return s.IsNull();
+        }
+
+        [TestCase("Kate", ExpectedResult = false)]
+        [TestCase(null, ExpectedResult = true)]
+        public static bool SimpleStrTest(string s)
+        {
+            return s.IsNull();
+        }
+    }
+}
