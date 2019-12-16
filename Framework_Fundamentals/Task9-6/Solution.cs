9b9e2b739b4243fe3c8757e79d89c35a5ffa979d
Ravil <ravlikfure@gmail.com>
2019-12-17 03:24:27 +0400

    Task9-6: solution realized

---
+++ Framework_Fundamentals/Task9-6/Solution.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task9_6
+{
+    public class Solution
+    {
+        public static string AddBigNumbers(string a, string b)
+        {
+            var biggerNum = a.Length >= b.Length ? a : b;
+            var lesserNum = a.Length < b.Length ? a : b;
+
+            var result = new StringBuilder(biggerNum.Length+1);
+            int cache = 0;
+            for(int i = 0; i<biggerNum.Length; i++)
+            {
+                var num = int.Parse(biggerNum.Substring(biggerNum.Length - i - 1, 1));
+                if (i<lesserNum.Length) num += int.Parse(lesserNum.Substring(lesserNum.Length - i - 1, 1));
+                result.Insert(0,num % 10 + cache);
+                cache = num / 10;
+            }
+
+            return result.ToString();
+        }
+    }
+}
---
+++ Framework_Fundamentals/Task9-6/Tests.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace Task9_6
+{
+    [TestFixture]
+    class Tests
+    {
+        [TestCase("1","10", ExpectedResult = "11")]
+        [TestCase("10000000000000000000", "10", ExpectedResult = "10000000000000000010")]
+        public string SimpleTest(string a, string b)
+        {
+            return Solution.AddBigNumbers(a, b);
+        }
+    }
+}
