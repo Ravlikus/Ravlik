03ecf66c42ae9bf6f5652e258d3a7e523a5b4808
Ravil <ravlikfure@gmail.com>
2019-12-17 03:05:27 +0400

    Task9-5: solution realized

---
+++ Framework_Fundamentals/Task9-5/Solution.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task9_5
+{
+    public class Solution
+    {
+        public static string ReverseAllWords(string input)
+        {
+            return input.Split(' ').Reverse().Aggregate((x,y) => x+" "+y);
+        }
+    }
+}
---
+++ Framework_Fundamentals/Task9-5/Tests.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace Task9_5
+{
+    [TestFixture]
+    class Tests
+    {
+        [TestCase("Каждый охотник желает", ExpectedResult = "желает охотник Каждый")]
+        [TestCase("Каждый", ExpectedResult = "Каждый")]
+        public string SimpleTest(string input)
+        {
+            return Solution.ReverseAllWords(input);
+        }
+    }
+}
