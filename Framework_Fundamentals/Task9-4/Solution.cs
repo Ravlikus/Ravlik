18b5a4c06bb3d8c1cfd2e0b35ec7ea4f30cfa593
Ravil <ravlikfure@gmail.com>
2019-12-17 02:54:36 +0400

    Task9-4: solution realized

---
+++ Framework_Fundamentals/Task9-4/Solution.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task9_4
+{
+    public class  Solution<X> where X : IComparable
+    {
+        public static List<X> GetUniqueInOrder<T>(T data) where T : IEnumerable<X>
+        {
+            var result = new List<X>();
+            X cacheObj = data.First();
+            var flag = true;
+            foreach(X obj in data)
+            {
+                if (flag|| cacheObj.CompareTo(obj) != 0)
+                {
+                    result.Add(obj);
+                    cacheObj = obj;
+                    flag = false;
+                }
+            }
+            return result;
+        }
+    }
+}
---
+++ Framework_Fundamentals/Task9-4/Tests.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace Task9_4
+{
+    [TestFixture]
+    class Tests
+    {
+        [TestCase]
+        public void SimpleTests()
+        {
+            var s = new List<char>() { '1', '1', '2', '2', '3', '3' };
+            Assert.AreEqual(new List<char>() { '1', '2', '3' }, Solution<char>.GetUniqueInOrder<List<char>>(s));
+            var str = "sssssAAacCCBBBb";;
+            Assert.AreEqual("sAacCBb", Solution<char>.GetUniqueInOrder<string>(str));
+        }
+    }
+}
