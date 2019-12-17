ee1ab4cf584f1c82fe914be1f482e796f2ea2029
Ravil <ravlikfure@gmail.com>
2019-12-17 19:39:29 +0400

    Task11-2: solution realized

---
+++ Delegates.Lambdas_and_Events/Task11-2/Solution.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task11_2
+{
+    public class Solution
+    {
+        private delegate RowData[] OrderCriterionMethodType(RowData[] data);
+        private delegate RowData[] SortCriterionMethodType(int[][] matrix);
+        public delegate int[][] MatrixSorter(int[][] matrix, SortByParam sortBy, OrderByParam orderBy);
+        public static MatrixSorter SortMatrix = Sort;
+
+
+        private static int[][] Sort(int[][] matrix, SortByParam sortBy, OrderByParam orderBy)
+        {
+            var resultRows = OrderMethods[orderBy](SortMethods[sortBy](matrix));
+            var result = new int[matrix.Length][];
+            for (int i = 0; i < matrix.Length; i++)
+            {
+                result[i] = matrix[resultRows[i].NumberByOrder];
+            }
+
+            return result;
+        }
+
+        private static Dictionary<OrderByParam, OrderCriterionMethodType> OrderMethods =
+            new Dictionary<OrderByParam, OrderCriterionMethodType>()
+            {
+                { OrderByParam.Ascending, OrderByAscending },
+                { OrderByParam.Descending, OrderByDescending }
+            };
+
+
+        private static Dictionary<SortByParam, SortCriterionMethodType> SortMethods =
+            new Dictionary<SortByParam, SortCriterionMethodType>()
+            {
+                { SortByParam.ByMaxValue, SortByMax },
+                { SortByParam.ByMinValue, SortByMin },
+                { SortByParam.BySum, SortBySum },
+            };
+
+        private static RowData[] SortByMax(int[][] matrix)
+        {
+            var matrixRowsData = new RowData[matrix.Length];
+            for (int i = 0; i < matrix.Length; i++)
+            {
+                var rowData = new RowData();
+                rowData.NumberByOrder = i;
+                rowData.SortValue = matrix[i].Max();
+                matrixRowsData[i] = rowData;
+            }
+            return matrixRowsData;
+        }
+
+        private static RowData[] SortByMin(int[][] matrix)
+        {
+            var matrixRowsData = new RowData[matrix.Length];
+            for (int i = 0; i < matrix.Length; i++)
+            {
+                var rowData = new RowData();
+                rowData.NumberByOrder = i;
+                rowData.SortValue = matrix[i].Min();
+                matrixRowsData[i] = rowData;
+            }
+            return matrixRowsData;
+        }
+        private static RowData[] SortBySum(int[][] matrix)
+        {
+            var matrixRowsData = new RowData[matrix.Length];
+            for (int i = 0; i < matrix.Length; i++)
+            {
+                var rowData = new RowData();
+                rowData.NumberByOrder = i;
+                rowData.SortValue = matrix[i].Sum();
+                matrixRowsData[i] = rowData;
+            }
+            return matrixRowsData;
+        }
+
+        private static RowData[] OrderByAscending(RowData[] data)
+        {
+            for (int i = 1; i < data.Length; i++)
+            {
+                for (int j = i; j > 0; j--)
+                {
+                    if (data[j].SortValue < data[j - 1].SortValue)
+                    {
+                        var container = data[j];
+                        data[j] = data[j - 1];
+                        data[j - 1] = container;
+                    }
+                }
+            }
+            return data;
+        }
+
+        private static RowData[] OrderByDescending(RowData[] data)
+        {
+            for (int i = 1; i < data.Length; i++)
+            {
+                for (int j = i; j > 0; j--)
+                {
+                    if (data[j].SortValue > data[j - 1].SortValue)
+                    {
+                        var container = data[j];
+                        data[j] = data[j - 1];
+                        data[j - 1] = container;
+                    }
+                }
+            }
+            return data;
+        }
+
+        private class RowData
+        {
+            public int NumberByOrder;
+            public int SortValue;
+        }
+
+        public enum SortByParam
+        {
+            BySum,
+            ByMaxValue,
+            ByMinValue
+        }
+
+        public enum OrderByParam
+        {
+            Ascending,
+            Descending
+        }
+    }
+}
---
+++ Delegates.Lambdas_and_Events/Task11-2/Tests.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace Task11_2
+{
+    [TestFixture]
+    public class Tests
+    {
+        static int[][] MatrixExample = new int[][] { new int[] { 1, 2, 3 }, new int[] { 2, 3, 4 }, new int[] { 3, 4, 5 } };
+
+        [TestCase(Solution.SortByParam.ByMaxValue, Solution.OrderByParam.Ascending, ExpectedResult = true)]
+        [TestCase(Solution.SortByParam.ByMaxValue, Solution.OrderByParam.Descending, ExpectedResult = true)]
+        [TestCase(Solution.SortByParam.ByMinValue, Solution.OrderByParam.Ascending, ExpectedResult = true)]
+        [TestCase(Solution.SortByParam.ByMinValue, Solution.OrderByParam.Descending, ExpectedResult = true)]
+        [TestCase(Solution.SortByParam.BySum, Solution.OrderByParam.Ascending, ExpectedResult = true)]
+        [TestCase(Solution.SortByParam.BySum, Solution.OrderByParam.Descending, ExpectedResult = true)]
+        public bool SimpleSortTests(Solution.SortByParam sortBy, Solution.OrderByParam orderBy)
+        {
+            var matrix = MatrixExample;
+            matrix = Solution.SortMatrix(matrix, sortBy, orderBy);
+            switch (sortBy)
+            {
+                case Solution.SortByParam.ByMaxValue:
+                    if (orderBy == Solution.OrderByParam.Ascending)
+                        return CompareMatrix(matrix, MatrixExample.OrderBy(x => x.Max()).ToArray());
+                    else return CompareMatrix(matrix, MatrixExample.OrderByDescending(x => x.Max()).ToArray());
+                case Solution.SortByParam.ByMinValue:
+                    if (orderBy == Solution.OrderByParam.Ascending)
+                        return CompareMatrix(matrix, MatrixExample.OrderBy(x => x.Min()).ToArray());
+                    else return CompareMatrix(matrix, MatrixExample.OrderByDescending(x => x.Min()).ToArray());
+                case Solution.SortByParam.BySum:
+                    if (orderBy == Solution.OrderByParam.Ascending)
+                        return CompareMatrix(matrix, MatrixExample.OrderBy(x => x.Sum()).ToArray());
+                    else return CompareMatrix(matrix, MatrixExample.OrderByDescending(x => x.Sum()).ToArray());
+                default: return false;
+            }
+        }
+
+        private bool CompareMatrix(int[][] first, int[][] second)
+        {
+            var flag = true;
+            for (int i = 0; i < first.Length; i++)
+            {
+                for (int j = 0; j < first[i].Length; j++)
+                {
+                    if (first[i][j] != second[i][j])
+                    {
+                        flag = false;
+                        break;
+                    }
+                }
+                if (!flag)
+                    break;
+            }
+
+            return flag;
+        }
+    }
+}
