8098ee24e1b961a62c436ce584c03a1685633146
Ravil <ravlikfure@gmail.com>
2019-12-17 18:32:03 +0400

    Task11-1: solution realized

---
+++ Delegates.Lambdas_and_Events/Task11-1/Solution.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task11_1
+{
+    public class Solution
+    {
+        public delegate int GSDFinder(params int[] values);
+        public delegate TimeSpan TimeChecker(params int[] values);
+
+
+        public static class EuclidianGCDFinder // НОД 
+        {
+            public static GSDFinder FindGSD = GetGSDByValues;
+            public static TimeChecker GetGSDFindingTiming = GetTiming;
+
+            /// <summary>
+            /// Метод, замеряющий время работы метода FindGCD с задаными входными параметрами
+            /// </summary>
+            private static TimeSpan GetTiming(params int[] values)
+            {
+                var timer = new Stopwatch();
+                timer.Start();
+                var s = GetGSDByValues(values);
+                timer.Stop();
+                return timer.Elapsed;
+            }
+
+            /// <summary>
+            /// Метод ищет НОД всех входящих чисел
+            /// </summary>
+            private static int GetGSDByValues(params int[] values)
+            {
+                var result = GetGSDByValues(values[0], values[1]);
+                for (int i = 2; i < values.Length; i++)
+                {
+                    result = GetGSDByValues(result, values[i]);
+                }
+                return result;
+            }
+
+            /// <summary>
+            /// Метод ищет НОД 2-х чисел
+            /// </summary>
+            private static int GetGSDByValues(int a, int b)
+            {
+                if (a == 0) return b;
+                if (b == 0) return a;
+                a = Math.Abs(a);
+                b = Math.Abs(b);
+                if (a == b) return a;
+                var min = Math.Min(a, b);
+                var diff = Math.Abs(a - b);
+                return GetGSDByValues(min, diff);
+            }
+        }
+
+        public static class SteinGCDFinder
+        {
+            public static GSDFinder FindGSD = GetGSDByValues;
+            public static TimeChecker GetGSDFindingTiming = GetTiming;
+
+            /// <summary>
+            /// Метод, замеряющий время работы метода FindGCD с задаными входными параметрами
+            /// </summary>
+            private static TimeSpan GetTiming(params int[] values)
+            {
+                var timer = new Stopwatch();
+                timer.Start();
+                var s = GetGSDByValues(values);
+                timer.Stop();
+                return timer.Elapsed;
+            }
+
+            /// <summary>
+            /// Метод ищет НОД всех входящих чисел
+            /// </summary>
+            private static int GetGSDByValues(params int[] values)
+            {
+                var result = GetGSDByValues(values[0], values[1]);
+                for (int i = 2; i < values.Length; i++)
+                {
+                    result = GetGSDByValues(result, values[i]);
+                }
+                return result;
+            }
+
+            /// <summary>
+            /// Метод ищет НОД 2-х чисел
+            /// </summary>
+            private static int GetGSDByValues(int a, int b)
+            {
+                if (a == 0) return b;
+                if (b == 0) return a;
+                if (a == b) return a;
+                if (a == 1) return 1;
+                if (b == 1) return 1;
+                if (a % 2 == 0 && b % 2 == 0) return 2 * GetGSDByValues(a / 2, b / 2);
+                if (a % 2 == 0 && b % 2 != 0) return GetGSDByValues(a / 2, b);
+                if (a % 2 != 0 && b % 2 == 0) return GetGSDByValues(a, b / 2);
+                if (a > b) return GetGSDByValues((a - b) / 2, b);
+                return GetGSDByValues((b - a) / 2, a);
+            }
+        }
+    }
+}
---
+++ Delegates.Lambdas_and_Events/Task11-1/Tests.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit;
+using NUnit.Framework;
+
+namespace Task11_1
+{
+    [TestFixture]
+    public class SolutionTests
+    {
+        [TestCase(4, 6, ExpectedResult = 2 )]
+        [TestCase(4, 6, 8, ExpectedResult = 2)]
+        [TestCase(8, 8, 8, ExpectedResult = 8)]
+        [TestCase(0, 0, 0, ExpectedResult = 0)]
+        [TestCase(0, 0, 1, ExpectedResult = 1)]
+        [TestCase(-4, 6, 8, ExpectedResult = 2)]
+        [TestCase(15, 18, 10, 5, ExpectedResult = 1)]
+        public int EuclidianAlgSimpleTests(params int[] values)
+        {
+            return Solution.EuclidianGCDFinder.FindGSD(values);
+        }
+
+        [TestCase(4, 6, ExpectedResult = 2)]
+        [TestCase(4, 6, 8, ExpectedResult = 2)]
+        [TestCase(8, 8, 8, ExpectedResult = 8)]
+        [TestCase(0, 0, 0, ExpectedResult = 0)]
+        [TestCase(0, 0, 1, ExpectedResult = 1)]
+        [TestCase(-4, 6, 8, ExpectedResult = 2)]
+        [TestCase(15, 18, 10, 5, ExpectedResult = 1)]
+        public int SteinsAlgSimpleTests(params int[] values)
+        {
+            return Solution.SteinGCDFinder.FindGSD(values);
+        }
+    }
+}
