ef1bb9fb1e4160fa489a16b2cde90e9aa489df63
Ravil <ravlikfure@gmail.com>
2019-12-18 16:13:59 +0400

    Task11-3: solution realized

---
+++ Delegates.Lambdas_and_Events/Task11-3/Solution.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Task11_3
+{
+    public delegate void MessageDelegate(string message);
+    public delegate void CounterDelegate(string message, int time);
+    public class Countdown
+    {
+        public event MessageDelegate CountEndTrigger;
+
+        public void StartCounting(string message, int time)
+        {
+            Task.Run(() => CountAndTrigger(message,time));
+        }
+
+        private void CountAndTrigger(string message, int time)
+        {
+            Thread.Sleep(time);
+            CountEndTrigger.Invoke(message);
+        }
+    }
+}
---
+++ Delegates.Lambdas_and_Events/Task11-3/Tests.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task11_3
+{
+    class Program
+    {
+        private delegate void MsgDelegate(string msg);
+
+        static void Main()
+        {
+            var counter = new Countdown();
+            MessageDelegate firstMethod = (x) => { Console.WriteLine($"First method message:{x}"); };
+            MessageDelegate secondMethod = (x) => { Console.WriteLine($"Second method message:{x}"); };
+            counter.CountEndTrigger += firstMethod;
+            counter.CountEndTrigger += secondMethod;
+            counter.StartCounting("First call",1000);
+            counter.StartCounting("Second call", 500);
+            Console.ReadKey();
+        }
+    }
+}
