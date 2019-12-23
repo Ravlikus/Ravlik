b897bb8f64dc917887c2c68cde30c4a97eb3c7fd
Ravil <ravlikfure@gmail.com>
2019-12-23 05:19:14 +0400

    Task12-1: solution realized

---
+++ Generics_And_Collections/Task12-1/Solution.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task12_1
+{
+    class Solution
+    {
+        public static int FindElement<T>(T[] data, T neededElement, Comparer<T> comparer)
+        {
+            if (data[0].Equals(neededElement)) return 0;
+            var bottomBorder = 0;
+            var topBorder = data.Length - 1;
+
+            while (bottomBorder <= topBorder)
+            {
+                var middle = (topBorder + bottomBorder) / 2;
+                if (comparer.Compare(data[middle], neededElement) == 0) return middle;
+                else if (comparer.Compare(data[middle], neededElement) > 0) { topBorder = middle ; }
+                else bottomBorder = middle + 1 ;
+            }
+
+            throw new Exception("Element not found!");
+        }
+    }
+}
---
+++ Generics_And_Collections/Task12-1/Tests.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace Task12_1
+{
+    [TestFixture]
+    public class Tests
+    {
+        [TestCase]
+        public void BinarySearchFunctionalTest()
+        {
+            var rnd = new Random();
+            var comparer = Comparer<int>.Create(new Comparison<int>((a, b) => a - b));
+            for (int i = 0; i < 10000; i++)
+            {
+                var elementsCount = rnd.Next(1, 10000);
+                var neededPosition = rnd.Next(1, elementsCount);
+                var neededValue = rnd.Next(1, int.MaxValue / 2);
+                var input = new List<int>();
+                for (int j = 0; j < elementsCount; j++)
+                {
+                    if (j < neededPosition) input.Add(rnd.Next(neededValue));
+                    else if (j > neededPosition) input.Add(rnd.Next(neededValue, int.MaxValue));
+                    else input.Add(neededValue);
+                }
+                input.Sort();
+                Assert.AreEqual(neededPosition, Solution.FindElement(input.ToArray(), neededValue, comparer));
+            }
+        }
+    }
+}
